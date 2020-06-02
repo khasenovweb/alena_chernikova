89e45ff6a93b226bd683b92d529176ff8c98cb46
khasenovweb <hasenovkirill93@gmail.com>
2020-06-02 22:19:27 +0500

    jhdjfh

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,36 @@
- 
+$(document).ready(function(){
+	$("a[data-role='scroll']").mPageScroll2id({
+		offset: 96
+	});
+
+
+	$.validator.addMethod(
+	        "phone",
+	        function (value) {
+	            return value.replace(/\D+/g, "").length >= 11;
+	        },
+	        "Введите номер телефона полностью"
+	    );
+
+	    $("form[data-validate]").each(function (i, el) {
+	        $(el).validate({
+	            rules: {
+	                lead_phone: "phone",
+	                lead_email: {
+	                	required: true,
+	                	email: true
+	                }
+	            },
+	            messages: {
+	            	lead_email: {
+	                	required: "Заполните поле E-mail",
+	                	email: "Введите корректно E-mail"
+	                }
+	            }
+	        });
+	    });
+
+
+
+	$('input[name="lead_phone"]').mask('+7 (999) 999-9999');
+});
